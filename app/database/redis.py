539ef6fe448402cbb5ddc80ed3cc2076535ba7af
Idris David <idris.david@lmu.edu.ng>
2025-11-24 10:49:46 +0100

    made redis available for depolyed api as well as for local testing

--- app/database/redis.py
+++ app/database/redis.py
@@ -1,11 +1,17 @@
 from redis.asyncio import Redis
 from app.config import database_settings
+import os
 
-_token_blacklist = Redis(
-    host=database_settings.REDIS_HOST,
-    port=database_settings.REDIS_PORT,
-    db=0,
-)
+redis_url = Redis(os.getenv("REDIS_URL"))
+
+if redis_url:
+    _token_blacklist = Redis.from_url(os.getenv("REDIS_URL"))
+else:
+    _token_blacklist = Redis(
+        host=database_settings.REDIS_HOST,
+        port=database_settings.REDIS_PORT,
+        db=0,
+    )
 
 
 async def add_to_blacklist(jti: str):
